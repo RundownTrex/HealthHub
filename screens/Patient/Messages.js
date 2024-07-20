fb745f370a20dec5d6cbd11baf0a62fc7b550a7e
RundownTrex <46619689+RundownTrex@users.noreply.github.com>
2024-07-20 23:57:56 +0530

    Initial commit

---
+++ screens/Patient/Messages.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Messages() {
+  return (
+    <View>
+      <Text>Messages screen</Text>
+     </View>
+  );
+}
---
+++ utils/colors.js
@@ -0,0 +1,13 @@
+const colors = {
+  darkback: "#303030",
+  blacktext: "#000000",
+  lightgraytext: "#CCCCCC",
+  whitetext: "#FFFFFF",
+  darkgraytext: "#999999",
+
+  accent: "#FF5722",
+  complementary: "#008597",
+  lightaccent: "#E1882A",
+};
+
+export default colors;
